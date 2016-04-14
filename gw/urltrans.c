8a93f6472e28676e0586f51f2e25afac2324313c
stolj <stolj@a7d65f57-29cd-40c9-b71e-ebca51ad1b1d>
2016-04-14 15:47:38 +0000

    2016-04-14  Stipe Tolj  <stolj at kannel.org>     * doc/userguide/userguide.xml: add documentation for escape code '%e'.     * gw/urltrans.c: add escape code '%e' for printable hexadecimal byte codes.
    git-svn-id: https://svn.kannel.org/gateway/trunk@5162 a7d65f57-29cd-40c9-b71e-ebca51ad1b1d

--- gw/urltrans.c
+++ gw/urltrans.c
@@ -381,7 +381,7 @@ Octstr *urltrans_fill_escape_codes(Octstr *pattern, Msg *request)
         }
         break;
 
-    case 'b':
+    case 'b':   /* message payload, URL-encoded */
         enc = octstr_duplicate(request->sms.msgdata);
         octstr_url_encode(enc);
         octstr_append(result, enc);
@@ -432,6 +432,15 @@ Octstr *urltrans_fill_escape_codes(Octstr *pattern, Msg *request)
         if (octstr_len(request->sms.meta_data)) {
             enc = octstr_duplicate(request->sms.meta_data);
             octstr_url_encode(enc);
+            octstr_append(result, enc);
+            octstr_destroy(enc);
+        }
+        break;
+
+    case 'e':   /* message payload, printable hexadecimal byte codes */
+        if (octstr_len(request->sms.msgdata)) {
+            enc = octstr_duplicate(request->sms.msgdata);
+            octstr_binary_to_hex(enc, 1);
             octstr_append(result, enc);
             octstr_destroy(enc);
         }
